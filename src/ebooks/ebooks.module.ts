762a99ec650ea3fec92b47ac8d6a3c02bbbaed81
Samhernesp <samhernesp@gmail.com>
2024-04-17 15:00:46 -0500

    Fix some bugs

--- src/ebooks/ebooks.module.ts
+++ src/ebooks/ebooks.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EbooksService } from './ebooks.service';
 import { EbooksController } from './ebooks.controller';
 import { Ebook } from './entities/ebook.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ebook])],
+  imports: [TypeOrmModule.forFeature([Ebook]), 
+            AuthModule],
   controllers: [EbooksController],
   providers: [EbooksService],
   exports: [EbooksService],
