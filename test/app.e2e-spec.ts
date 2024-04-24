29dd609d9bec68c72c03a2ff2bc8a607bdfba117
Samhernesp <samhernesp@gmail.com>
2024-04-24 08:59:26 -0500

    Add e2e test for ebook controller

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -8,6 +8,9 @@ import { AuthController } from '../src/auth/auth.controller';
 import { AuthService } from '../src/auth/auth.service';
 import { RolesGuard } from '../src/auth/guard/roles.guard';
 import { RoleEnum } from '../src/auth/enum/role.enum';
+import { EbooksService } from '../src/ebooks/ebooks.service';
+import { EbooksController } from '../src/ebooks/ebooks.controller';
+import { DeleteResult } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -143,6 +146,203 @@ describe('AuthController', () => {
       .send(updateUserDto)
       .expect(200)
       .expect(updatedUser);
+  });
+  
+});
+
+describe('EbooksController (e2e)', () => {
+  let app: INestApplication;
+  let ebooksService: EbooksService;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      controllers: [EbooksController],
+      providers: [
+        {
+          provide: EbooksService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findById: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+            assignEbookToReader: jest.fn(),
+            findAllEbooksByReader: jest.fn(),
+            findByTitle: jest.fn(),
+          },
+        },
+        {
+          provide: AuthGuard('jwt'),
+          useValue: { canActivate: () => true },
+        },
+        {
+          provide: RolesGuard,
+          useValue: { canActivate: () => true },
+        },
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+    ebooksService = moduleFixture.get<EbooksService>(EbooksService);
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('POST /ebooks - should create an ebook', async () => {
+    const newEbookDto = {
+      title: "Example Book",
+      publisher: "Example Publisher",
+      author: "Author Name",
+      overview: "Detailed description of the book",
+      price: 19.99,
+      stock: 100
+    };
+
+    const expectedEbook = {
+      ...newEbookDto,
+      id: "4908705d-b880-467d-9ad6-f81b0138d8bc",
+    };
+
+    ebooksService.create = jest.fn().mockResolvedValue(expectedEbook);
+
+    const jwt = require('jsonwebtoken');
+
+    const token = jwt.sign(
+      {
+        sub: '4908705d-b880-467d-9ad6-f81b0138d8ba',
+        username: 'Author',
+        role: RoleEnum.AUTHOR
+      },
+      'clave_secreta',
+      { expiresIn: '1h' }
+    );
+  
+
+    await request(app.getHttpServer())
+      .post('/ebooks')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newEbookDto)
+      .expect(201)
+      .expect(expectedEbook);
+  });
+
+  it('GET /ebooks', async () => {
+    const ebooks = [{ title: 'Book 1' }, { title: 'Book 2' }];
+    ebooksService.findAll= jest.fn().mockResolvedValue(ebooks);
+
+    const jwt = require('jsonwebtoken');
+
+    const token = jwt.sign(
+      {
+        sub: '4908705d-b880-467d-9ad6-f81b0138d8ba',
+        username: 'Author',
+        role: RoleEnum.AUTHOR
+      },
+      'clave_secreta',
+      { expiresIn: '1h' }
+    );
+  
+  
+    await request(app.getHttpServer())
+      .get('/ebooks')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+      .expect(ebooks);
+  });
+
+  it('GET /ebooks/info/:id', async () => {
+    const author = { id:"4908705d-b880-467d-9ad6-f81b0138d8bh", userId: "4908705d-b880-467d-9ad6-f81b0138d8ba",penName: "Author",biography: "string",booksWritten: "string"}
+    const ebookInfo = { title: 'Book 1', publisher: 'Publisher', author: author, overview: 'Overview', price: 30, stock: 15 };
+    const expected = {title: 'Book 1', publisher: 'Publisher', author: author.penName, overview: 'Overview', price: 30, stock: 15 }
+    ebooksService.findById= jest.fn().mockResolvedValue(ebookInfo);
+    
+    const jwt = require('jsonwebtoken');
+
+    const token = jwt.sign(
+      {
+        sub: '4908705d-b880-467d-9ad6-f81b0138d8ba',
+        username: 'Author',
+        role: RoleEnum.AUTHOR
+      },
+      'clave_secreta',
+      { expiresIn: '1h' }
+    );
+
+    await request(app.getHttpServer())
+      .get('/ebooks/info/1')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+      .expect(expected);
+  });
+  
+  it('PATCH /ebooks/visualize/:id', async () => {
+    const ebookUpdateDto = {
+      id: "4908705d-b880-467d-9ad6-f81b0138d8bh",
+      title: "Example Book",
+      publisher: "Example Publisher",
+      author: "Author Name",
+      overview: "Detailed description of the book",
+      price: 19.99,
+      stock: 100
+    };
+    const updatedEbook = { ...ebookUpdateDto, description: 'Updated Description', price: 25 };
+    ebooksService.update = jest.fn().mockResolvedValue(updatedEbook);
+    ebooksService.findById = jest.fn().mockResolvedValue(ebookUpdateDto);
+    
+    const jwt = require('jsonwebtoken');
+
+    const token = jwt.sign(
+      {
+        sub: '4908705d-b880-467d-9ad6-f81b0138d8ba',
+        username: 'Author',
+        role: RoleEnum.AUTHOR
+      },
+      'clave_secreta',
+      { expiresIn: '1h' }
+    );
+
+    await request(app.getHttpServer())
+      .patch('/ebooks/visualize/4908705d-b880-467d-9ad6-f81b0138d8bh')
+      .set('Authorization', `Bearer ${token}`)
+      .send(ebookUpdateDto)
+      .expect(200)
+      .expect(updatedEbook);
+  });
+
+  it('DELETE /ebooks/:id', async () => {
+    const deleteResult = { affected: 1 } as DeleteResult;
+    const ebook = {
+      id: "4908705d-b880-467d-9ad6-f81b0138d8bc",
+      title: "Example Book",
+      publisher: "Example Publisher",
+      author: "Author Name",
+      overview: "Detailed description of the book",
+      price: 19.99,
+      stock: 100
+    };
+    ebooksService.remove = jest.fn().mockResolvedValue(deleteResult);
+    ebooksService.findById = jest.fn().mockResolvedValue(ebook)
+
+    const jwt = require('jsonwebtoken');
+
+    const token = jwt.sign(
+      {
+        sub: '4908705d-b880-467d-9ad6-f81b0138d8ba',
+        username: 'Author',
+        role: RoleEnum.AUTHOR
+      },
+      'clave_secreta',
+      { expiresIn: '1h' }
+    );
+  
+    await request(app.getHttpServer())
+      .delete('/ebooks/4908705d-b880-467d-9ad6-f81b0138d8bc')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+      .expect(deleteResult);
   });
   
 });
